b868715266144b2215eb55e99ce8e4db7209a7d1
Kevin <kevinator900@gmail.com>
2021-03-11 15:28:00 -0700

    finished singleplayer gameplay

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
     $(function(){
-    let hand = [];
     let handContainer = document.getElementById("hand")
+    let topCard = "";
     let deck = {
-        amount: 108,
         cards : ['r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'rPlus2', 'rReverse', 'rSkip', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'rPlus2', 'rReverse', 'rSkip','b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'bPlus2', 'bReverse', 'bSkip', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'bPlus2', 'bReverse', 'bSkip','g0', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'gPlus2', 'gReverse', 'gSkip', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'gPlus2', 'gReverse', 'gSkip','y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9', 'yPlus2', 'yReverse', 'ySkip', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9', 'yPlus2', 'yReverse', 'ySkip','BWild', 'BPlus4','BWild', 'BPlus4','BWild', 'BPlus4','BWild', 'BPlus4',],
         shuffle : function(cards = this.cards){
             let shufflingCards = [];
@@ -15,6 +14,7 @@
                     --i;
                 }
             }
+            console.log(shufflingCards)
             for(let i = 0; i < shufflingCards.length; i++){
                 this.drawPile.push(shufflingCards[i])
             }
@@ -22,12 +22,12 @@
 
         },
         drawPile: [],
-        usedCards: [],
+        discardPile: [],
+        topCard: "",
     }
     
-    function drawCard(amount = 1){
+    function drawCard(){
         let cardDrawn = deck.drawPile.shift();
-        hand.push(cardDrawn)
         
         let newCard = document.createElement("div");
         newCard.classList += "card"
@@ -55,17 +55,106 @@
         // Text on the card
         let text = cardDrawn.substring(1)
         newCard.innerHTML = text;
+        newCard.setAttribute("card", cardDrawn);
         // Onclick 
-        newCard.on("click", function(){playCard(cardDrawn)})
+        newCard.onclick = function(){playCard(cardDrawn)}
 
+        //Adding it in the right order
+        let sortNum = deck.cards.indexOf(cardDrawn);
+        newCard.setAttribute("sortNum", sortNum)
         handContainer.append(newCard);
+        newCard = $(`[card = ${cardDrawn}]`)
+        
+        let currHand = $('.card')
+        if(currHand.length == 0){
+            return;
+        }
+        for(let i = 0; i < currHand.length; i++){
+            let newSortNum = $(currHand[i]).attr('sortNum')
+            if(sortNum < newSortNum){
+                newCard.remove();
+                newCard.insertBefore(currHand[i])
+                break;
+            }
+        }
+        newCard.css("height", "14.4rem")
+        newCard.css("width", "9.6rem")
+        newCard.css("font-size", "2.4rem")
+        newCard.css("line-height", "14.4rem")
+        setTimeout(function(){
+            newCard.css("height", "12rem")
+            newCard.css("width", "8rem")
+            newCard.css("font-size", "2rem")
+            newCard.css("line-height", "12rem")
+        }, 300)
+
+
+    }
+
+    function drawCards(amount = 1, delay){
+        let interval = setInterval(drawCard, delay);
+        setTimeout(function(){clearInterval(interval)}, delay * amount)
     }
 
     function playCard(card){
-        console.log(card)
+        let topCard = deck.topCard;
+        let topColor = topCard.charAt(0);
+        let newColor = card.charAt(0);
+        let topType = topCard.substring(1);
+        let newType = card.substring(1);
+
+        if(topColor == newColor || topType == newType || newType == "Wild" || newType == "Plus4" || topType == "Wild" || topType == "Plus4"){
+            changeTopCard(card);
+            $(`[card = ${card}]`)[0].remove();
+        }
+        
+        if($('.card').length == 0){
+            alert("CONGRATULATIONS! YOU WIN!")
+        }
     }
 
-    deck.shuffle();
+    function changeTopCard(newCard){
+        deck.discardPile.push(deck.topCard)
+        deck.topCard = newCard;
+        let color = ""
+        $('.discardPile').css("color", 'white')
+        switch(newCard.charAt(0)){
+            case 'r':
+                color = 'red';
+                break;
+            case 'b':
+                color = 'blue';
+                break;
+            case 'y':
+                color = 'yellow';
+                $('.discardPile').css("color", 'black')
+                break;
+            case 'g':
+                color = 'green';
+                break;
+            case 'B':
+                color = 'black';
+                break;
+        } 
 
+        $('.discardPile').css("background-color", color).text(newCard.substring(1))
+
+    }
+
+
+    function startGame(){
+        deck.shuffle();
+        drawCards(7, 100);
+        let firstCard = deck.drawPile.shift();
+        changeTopCard(firstCard)
+    }
+
+    function reshuffle(){
+        deck.discardPile.shift()
+        deck.shuffle(deck.discardPile);
+        deck.discardPile = [];
+    }
+    startGame()
     $('.drawPile').on("click", drawCard)
+    $('#shuffle').on("click", reshuffle)
 })
